681a4d70038a703d69e9b7b0cfbed1bea9a44a97
CloneyMari <cloneymaricarandang@gmail.com>
2023-07-06 10:20:22 +0800

    add show, comments edit and update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,6 +19,19 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+      if @comment.update(comment_params)
+              flash[:notice] = 'Comment updated successfully'
+              redirect_to post_comments_path(@post)
+      else
+        render :edit
+      end
+    end
+
+
+
   private
 
   def set_post
@@ -28,6 +41,10 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content)
   end
+
+   def set_comment
+       @comment = @post.comments.find(params[:id])
+     end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   resources :posts do
-    resources :comments
+    resources :comments, except: :show
   end
 end
